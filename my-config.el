986ff08ecb278d0c97f26dd51fc6d195de48a7cb
Alex Robson <alex@sharplearningcurve.com>
2011-06-16 11:50:34 -0400

    Fixing the erlang-start script

--- my-config.el
+++ my-config.el
@@ -150,7 +150,7 @@
 (autoload 'markdown-mode "markdown-mode.el"
    "Major mode for editing Markdown files" t)
 (setq auto-mode-alist
-   (cons '("\\.md" . markdown-mode) auto-mode-alist))
+   (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
 
 
 ;;Run rebar post save
