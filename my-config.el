c9a43ef6030cbd32897328bc386a5bf8913d1f0a
Alex Robson <alex@sharplearningcurve.com>
2011-08-12 20:52:36 -0400

    Attempting to fix broken coffe-mode submodule and correct typo in config

--- my-config.el
+++ my-config.el
@@ -164,7 +164,7 @@
 (provide 'my-config)
 
 ;;COFFEE SCRIPT! AHAHAHA! I LOVE YOU
-(add-to-list 'load-path "~/emacs_libs/coffee-module")
+(add-to-list 'load-path "~/emacs_libs/coffee-mode")
 (require 'coffee-mode)
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
 (add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
